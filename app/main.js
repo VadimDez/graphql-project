0e33ffdb28a9cfdacb9126a31285c08123c85d39
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2016-10-11 18:16:50 +0200

    express and webpack

---
+++ app/main.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Vadym Yatsyuk on 10/10/2016
+ */
+var hello = document.querySelector('body');
+hello.innerHTML = 'Running...';
--- index.js
+++ index.js
@@ -5,9 +5,18 @@
 const express = require('express');
 const graphqlHttp = require('express-graphql');
 const schema = require('./schema');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
 
+const config = require('./webpack.config');
+const path = require('path');
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
+const isDevelopment = process.env.NODE_ENV !== "production";
+const compiler = webpack(config);
+const public_folder = path.join(__dirname, 'dist');
+const html_file = path.join(public_folder, 'index.html');
 
 app.use('/graphql', graphqlHttp({
   schema,
@@ -15,7 +24,28 @@ app.use('/graphql', graphqlHttp({
   graphiql: true
 }));
 
-app.use(express.static(__dirname + '/public'));
+if (isDevelopment) {
+  app.use(webpackDevMiddleware(compiler, {
+    publicPath: config.output.publicPath,
+    stats: {
+      colors: true
+    }
+  }));
+
+  app.use(webpackHotMiddleware(compiler, {
+    log: console.log
+  }));
+
+  app.get("*", (req, res) => {
+    res.write(compiler.outputFileSystem.readFileSync(html_file));
+    res.end();
+  });
+} else {
+  app.use(express.static(public_folder));
+
+  app.get("*", (req, res) => res.sendFile(html_file));
+}
+
 
 app.listen(PORT, () => {
   console.log('Server running...');
