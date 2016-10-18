b2f503a43c7b74ce18f21c7b88e029dde125ff16
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2016-10-18 13:15:41 +0200

    minor, init styling

--- app/app.js
+++ app/app.js
@@ -8,9 +8,20 @@ export class App extends React.Component {
   render() {
     return (
       <div>
-        <h1><Link to="/">GraphQL</Link></h1>
-        { this.props.children }
+        <header style={ styles.header }>
+          <h1><Link to="/">GraphQL</Link></h1>
+        </header>
+        <section>
+          { this.props.children }
+        </section>
       </div>
     );
   }
 }
+
+const styles = {
+  header: {
+    backgroundColor: '#e3e3e3',
+    padding: '10px'
+  }
+};
